eda3305d7891dfa0ef7d15770e585a215013aa8e
Bruno Gonçalves <btoztas@gmail.com>
2017-09-22 22:21:21 +0100

    fixed migrations

--- calendarapp/admin.py
+++ calendarapp/admin.py
@@ -2,11 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Event, StudentGroup, Course, Evaluation
+from .models import Event, StudentGroup, Degree, Evaluation
 
 # Register your models here.
 
 admin.site.register(Event)
 admin.site.register(StudentGroup)
-admin.site.register(Course)
+admin.site.register(Degree)
 admin.site.register(Evaluation)
