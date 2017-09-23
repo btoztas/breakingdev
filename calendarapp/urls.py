66ba616defacedd0d8b06751ce4ec25179d19d30
Bruno Gonçalves <btoztas@gmail.com>
2017-09-23 10:26:38 +0100

    updated urls

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^logout$', auth_views.logout, name='logout'),
     url(r'^register$', RegisterPageView.as_view(), name='register'),
     url(r'^forgot-password$', ForgotPasswordPageView.as_view(), name='resetPassword'),
-    url(r'^forgot-password.html$', ForgotPasswordPageView.as_view(), name='resetPassword'),
     url(r'^fullcalendar/', TemplateView.as_view(template_name="calendarapp/fullcalendar.html"), name='fullcalendar'),
     url(r'^fullcalendar2/', TemplateView.as_view(template_name="calendarapp/fullcalendar2.html"), name='fullcalendar'),
 ]
