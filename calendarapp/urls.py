7fc5942355ec0fe2482531d31009c7587c5d4c31
Rui Figueiredo <ruimgf95@gmail.com>
2017-09-23 11:25:38 +0100

    modify pages

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -5,11 +5,9 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^home$', HomePageView.as_view(), name='home'),
+    url(r'^home$', TemplateView.as_view(template_name="calendarapp/fullcalendar.html"), name='home'),
     url(r'^login$', auth_views.login, {'template_name': 'calendarapp/login.html'}, name='login'),
     url(r'^logout$', auth_views.logout, name='logout'),
     url(r'^register$', RegisterPageView.as_view(), name='register'),
-    url(r'^forgot-password$', ForgotPasswordPageView.as_view(), name='resetPassword'),
-    url(r'^fullcalendar/', TemplateView.as_view(template_name="calendarapp/fullcalendar.html"), name='fullcalendar'),
-    url(r'^fullcalendar2/', TemplateView.as_view(template_name="calendarapp/fullcalendar2.html"), name='fullcalendar'),
+    url(r'^forgot-password$', ForgotPasswordPageView.as_view(), name='resetPassword')
 ]
