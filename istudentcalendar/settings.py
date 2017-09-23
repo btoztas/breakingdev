c683a1f097ce84502b46e6d9c56f00723b1babd6
Bruno Gonçalves <btoztas@gmail.com>
2017-09-23 10:45:18 +0100

    fixed settings

--- istudentcalendar/settings.py
+++ istudentcalendar/settings.py
@@ -126,10 +126,10 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-#STATICFILES_DIRS = (
-#  os.path.join(BASE_DIR, 'static/'),
-#)
+#STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
