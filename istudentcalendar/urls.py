18f059d853f3f4763b40e3e554e67e4359e3dd0b
Rui Figueiredo <ruimgf95@gmail.com>
2017-09-23 09:56:31 +0100

    add /schedule

--- istudentcalendar/urls.py
+++ istudentcalendar/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^calendarapp/', include('calendarapp.urls')),
+    url(r'^schedule/', include('schedule.urls'), name='scheduler'),
 ]
