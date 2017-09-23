ad7ab409bbfd9a079cf603f142ef31d90d782f81
Bruno Gonçalves <btoztas@gmail.com>
2017-09-23 10:17:29 +0100

    finished basic register and login

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
-from .views import HomePageView, LoginPageView, RegisterPageView, ForgotPasswordPageView
+from .views import HomePageView, RegisterPageView, ForgotPasswordPageView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^index.html$', HomePageView.as_view(), name='home'),
-    url(r'^login.html$', LoginPageView.as_view(), name='login'),
-    url(r'^register.html$', RegisterPageView.as_view(), name='register'),
-    url(r'^forgot-password.html$', ForgotPasswordPageView.as_view(), name='resetPassword'),
+    url(r'^home$', HomePageView.as_view(), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'calendarapp/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^register$', RegisterPageView.as_view(), name='register'),
+    url(r'^forgot-password$', ForgotPasswordPageView.as_view(), name='resetPassword'),
 
 ]
