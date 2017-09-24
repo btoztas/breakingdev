fbf62aa880eea019001fdbcaf669b8335f863a45
martinho <pjemartinho@gmail.com>
2017-09-24 03:01:14 +0100

    ola

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -95,7 +95,7 @@ class EventView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         event_id = kwargs['event_id']
-        event = Event.objects.filter(id=event_id).first()
+        event = Event.objects.filter(pk=event_id).first()
         title = event.title
         description = event.description
 
